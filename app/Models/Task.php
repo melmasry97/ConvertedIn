c1373a619a239e11cfd3857c454c4de48bb6dfd5
mohamed elmasry <itsjust4me13@gmail.com>
2024-02-03 05:31:03 +0200

    create auth by laravel/auth ui

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+
+    /**
+     * Relationship: Task belongs to a user who is assigned to the task.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function assignedTo()
+    {
+        return $this->belongsTo(User::class, 'assigned_to_id');
+    }
+
+    /**
+     * Relationship: Task belongs to a user who assigned the task.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function assignedBy()
+    {
+        return $this->belongsTo(User::class, 'assigned_by_id');
+    }
 }
