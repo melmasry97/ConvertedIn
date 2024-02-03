e23be30dc17b0b979132e5137ce25a00f20f13fc
mohamed elmasry <itsjust4me13@gmail.com>
2024-02-03 09:57:09 +0200

    finish create command

---
+++ app/Console/Commands/CreateDatabase.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Validator;
+
+class CreateDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:create {name?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $databaseName = $this->argument('name') ?: config('database.connections.' . config('database.default') . '.database');
+
+        $validator = Validator::make(['name' => $databaseName], [
+            'name' => ['required', 'max:255', 'regex:/^[a-zA-Z0-9_]+$/'],
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error("Database $databaseName not created:" . $validator->errors()->first('name'));
+        }
+
+        if ($this->databaseExists($databaseName)) {
+            return $this->error("Database $databaseName already exists.");
+        }
+
+        $this->createDatabase($databaseName);
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * check if there is a DB with provieded name
+     * @param string $databaseName
+     * @return bool
+     */
+    protected function databaseExists($databaseName)
+    {
+        $existingDatabases = DB::select("SHOW DATABASES WHERE `Database` = '$databaseName'");
+        return count($existingDatabases) > 0;
+    }
+
+    /**
+     * create new Database
+     */
+    protected function createDatabase($databaseName)
+    {
+        $charset = config('database.connections.' . config('database.default') . '.charset') ?: 'utf8mb4';
+        $collation = config('database.connections.' . config('database.default') . '.collation') ?: 'utf8mb4_unicode_ci';
+
+        DB::statement("CREATE DATABASE $databaseName CHARACTER SET $charset COLLATE $collation");
+
+        Config::set("database.connections." . config('database.default') . ".database", $databaseName);
+
+        $this->info("Database $databaseName created successfully.");
+    }
+}
