edc330a3dbea33b9307c56e680377e27a0a7d058
mohamed elmasry <itsjust4me13@gmail.com>
2024-02-03 03:17:42 +0200

    create repo for general CURD operation

--- app/Repositories/GeneralRepository.php
+++ app/Repositories/GeneralRepository.php
@@ -34,18 +34,10 @@ class GeneralRepository
     {
         $data = $input;
         try {
-            if (isset($input['attachOne'])) {
-                $width = isset($input['attachOne']['width']) ? $input['attachOne']['width'] : null;
-                $height = isset($input['attachOne']['height']) ? $input['attachOne']['height'] : null;
-                $data[$input['attachOne']['name']] = $this->upload($input['attachOne']['file'], 'storage/' . $input['attachOne']['folder'], $width, $height);
-                unset($data['attachOne']);
-            }
+
             return $model::firstOrCreate($data);
         } catch (\Throwable $th) {
             throw $th;
-            if (isset($input['attachOne'])) {
-                $this->deleteImage($data[$input['attachOne']['name']]);
-            }
             return null;
         }
     }
@@ -53,29 +45,6 @@ class GeneralRepository
     public function create($model, $input)
     {
         return $model::create($input);
-    }
-
-    public function put($model, $conditions, $input)
-    {
-        $data = $input;
-        $row = $model::where($conditions)->first();
-        try {
-            if (isset($input['attachOne'])) {
-                $width = isset($input['attachOne']['width']) ? $input['attachOne']['width'] : null;
-                $height = isset($input['attachOne']['height']) ? $input['attachOne']['height'] : null;
-                $data[$input['attachOne']['name']] = $this->upload($input['attachOne']['file'], 'storage/' . $input['attachOne']['folder'], $width, $height); //public/' . $input['attachOne']['folder'] . '/
-                $name = $input['attachOne']['name'];
-                $this->deleteImage($row->$name);
-                unset($data['attachOne']);
-            }
-
-            $row->update($data);
-            return $row;
-        } catch (\Throwable $th) {
-            if (isset($input['attachOne'])) {
-                $this->deleteImage($data[$input['attachOne']['name']]);
-            }
-        }
     }
 
     public function find($model, $conditions)
@@ -95,51 +64,4 @@ class GeneralRepository
         return $row;
     }
 
-
-    public function upload($image, $folder = 'conferences', $with = null, $height = null)
-    {
-        // $path = $image->hashName($folder . '/' . Auth::user()->id);
-        // $image = Image::make($image)->fit(300);
-        // Storage::put($path, (string) $image->encode());
-        // return $url = Storage::url($path);
-
-        if (!empty($with) && !empty($height)) {
-            try {
-
-                if (!file_exists($folder)) {
-                    mkdir($folder, 0777, true);
-                }
-                //code...
-            } catch (\Throwable $th) {
-                $folder = 'images';
-            }
-            $path = $image->hashName($folder);
-            $image = Image::make($image)->resize($with, $height);
-            $image->save($path);
-            return $path;
-        } else {
-            $image_name = time() . rand(1, 100000) . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path($folder), $image_name);
-            return $folder . '/' . $image_name;
-        }
-    }
-
-    public function deleteImage($path)
-    {
-        $file = explode('public/', $path);
-        $file_name = end($file);
-        // File::delete(public_path($file_name));
-        File::delete($file_name);
-    }
-
-
-    public function visit_login($conference_id)
-    {
-        return Visit::where('conference_id', $conference_id)->where('user_id', '!=', null)->get();
-    }
-
-    public function add_visitor($request)
-    {
-        return $request->ip();
-    }
 }
